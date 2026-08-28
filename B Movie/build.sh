a11c4c53bd8fb4ff42004805b6585b0f6f3b2ce3
BartolelliL <leonardo.bartolelli@gmail.com>
2026-08-28 16:24:10 +0200

    28/08/26 build

--- B Movie/build.sh
+++ B Movie/build.sh
@@ -2,9 +2,11 @@
 # exit on error
 set -o errexit
 
-# Installazione istantanea delle dipendenze da uv.lock
+pip install uv
+
+# sync dependences
 uv sync --frozen
 
-# Esecuzione dei comandi all'interno dell'ambiente di uv
+# static files & db migration
 uv run python manage.py collectstatic --no-input
 uv run python manage.py migrate
