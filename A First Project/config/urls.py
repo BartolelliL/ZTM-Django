b271e97d55646c1858fe2b47ca21d414db5ee358
BartolelliL <leonardo.bartolelli@gmail.com>
2026-08-28 10:32:38 +0200

    28/07/26

--- A First Project/config/urls.py
+++ A First Project/config/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from my_app.views import index, about, hello, addition
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", index),
+    path("about/", about),
+    path("hello/<str:first_name>/", hello),
+    path("pizza/<int:num_1>/<int:num_2>/", addition)
 ]
--- A First Project/manage.py
+++ A First Project/manage.py
@@ -9,6 +9,7 @@ import sys
 # uv init --bare
 # uv add django
 # uv run django-admin startproject config .
+# uv run python manage.py startapp {APP-NAME}
 
 # To run it:
 # uv run python manage.py runserver
