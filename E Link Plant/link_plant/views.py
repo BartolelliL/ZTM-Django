c84cd8b5678bcb2157c7025193171177d69a1d52
BartolelliL <leonardo.bartolelli@gmail.com>
2026-09-03 09:01:48 +0200

    02/09/26

--- E Link Plant/link_plant/views.py
+++ E Link Plant/link_plant/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 from .models import Profile, Link
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView
+from django.urls import reverse_lazy
 
 class LinkListView(ListView):
     model = Link
+
+class LinkCreateView(CreateView):
+    model = Link
+    fields = "__all__"
+    success_url = reverse_lazy("link-list")
+    
+    def get(self, request):
+        return render(request, "link_plant/link_form.html")
+
+    def post(self, request):
+        pass
